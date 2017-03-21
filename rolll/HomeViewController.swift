d898a87344e6281d4f42b42b2bd5588f324599bc
carmen-c <carmen.csmwc@gmail.com>
2017-03-21 14:37:26 -0700

    change homescreen

--- rolll/HomeViewController.swift
+++ rolll/HomeViewController.swift
@@ -7,81 +7,27 @@
 //
 
 import UIKit
-import GTProgressBar
 
 class HomeViewController: UIViewController {
 
     //MARK: - Properties -
 
-    var isSleeping = false
-    var stamina:Float = 0.01
     
     //MARK: - Views -
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let progressBar = GTProgressBar(frame: CGRect(x: 20, y: 40, width: 200, height: 20))
-        progressBar.progress = CGFloat(stamina)
-        progressBar.barBorderColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
-        progressBar.barFillColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
-        progressBar.barBackgroundColor = UIColor.white
-        progressBar.barBorderWidth = 1
-        progressBar.barFillInset = 3
-        progressBar.barMaxHeight = 20
-        progressBar.displayLabel = false
-        
-        view.addSubview(progressBar)
-        
-//        progressBar.animateTo(progress: userStamina)
         
     }
     
     
     //MARK: - Buttons -
     
-    @IBAction func bed(_ sender: Any) {
-        if isSleeping == false {
-            isSleeping = true
-            
-        }else if isSleeping == true{
-            isSleeping = false
-        }
-        
-        sleep()
-        print("bed button pressed")
-    }
-
-    @IBAction func laptop(_ sender: Any) {
-        print("laptop button pressed")
-    }
     
-    @IBAction func door(_ sender: Any) {
-        print("door button pressed")
-    }
     
     
     //MARK: - Functions -
-    
-    func sleep() {
-        let timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(addStamina), userInfo: nil, repeats: true)
-        
-        if isSleeping == true {
-            timer.fire()
-        }else {
-            timer.invalidate()
-        }
-    }
-    
-    func addStamina() {
-        let increment:Float = 0.01
-        
-        if self.stamina < 1 {
-            self.stamina += increment
-        } else {
-            self.stamina += 0
-        }
-    }
 
     
     override func didReceiveMemoryWarning() {
